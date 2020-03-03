373d316e887f37ffc22566d2a40087f896759a68
Luzzi Charly <luzzi.charly@gmail.com>
2020-03-03 11:53:02 +0100

    'init'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Hello World</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Welcome To @ionic/vue</h1>
+      <img alt="Vue logo" src="../assets/logo.png">
+    </ion-content>
   </div>
 </template>
 
